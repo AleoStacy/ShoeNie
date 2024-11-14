9018f549b7afd0d86947d5e6e16fa61ffdc5371c
Maina <smurkcarter855@gmail.com>
2024-11-14 10:55:25 +0300

    Save local changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-// import { useState } from 'react'
+import React from "react";
+import SearchBar from "./component/SearchBar";
+import ContactPage from "./component/ContactPage";
+import AuthForm  from "./component/AuthenForm";
+import Footer from "./component/Footer";
 
-import './App.css'
+
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <div className="min-h-screen bg-gray-100">
+      <div className="container mx-auto py-8 px-4">
+        <div className="flex justify-center mb-8">
+          <SearchBar />
+        </div>
+        
+        <div className="mb-8">
+          <AuthForm />
+        </div>
+
+        <ContactPage />
+      </div>
+      <div>
+      <Footer />
+      </div>
+      </div>
   )
 }
 
-export default App
+        
+
+export default App;
